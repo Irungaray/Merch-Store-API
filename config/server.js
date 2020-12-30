4cc229344db16bed2165ba75720e974b14c70267
Irungaray <lionelirungaray@gmail.com>
2020-12-29 21:42:37 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4fa1e32371974ff96427fdfdbb7274bd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb714cac-55ff-45fb-aba2-3bd2ddd42d38'
+};
